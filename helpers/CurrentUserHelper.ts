d438265a3d4d9221749b6ff9d4bf9c1eaee85c9d
vileven <vileven11@icloud.com>
2017-06-07 22:25:03 +0300

    Added students info

--- helpers/CurrentUserHelper.ts
+++ helpers/CurrentUserHelper.ts
@@ -49,7 +49,34 @@ export class CurrentUserHelper {
                         case 1:
                             (<IModel> user).jsonParser.parseURLAsync(JSWorks.config['backendURL'] + '/student/info',
                                 JSWorks.HTTPMethod.GET)
-                                .then(data => resolve(data));
+                                .then(data => {
+                                    console.log(data);
+                                    data = [
+                                        {
+                                            course_id: 1,
+                                            course_name: 'technopark 2 sem',
+                                            group_id: 1,
+                                            group_name: "AПО-21",
+                                            subjects: [
+                                                {subject_id: 1, subject_name: "DataBase", total: 80, mark_name: 'хор'},
+                                                {subject_id: 2, subject_name: "Front-end", total: 100, mark_name: 'отл'},
+                                                {subject_id: 3, subject_name: "Back-end", total: 50, mark_name: 'удовл'}
+                                            ]
+                                        },
+                                        {
+                                            course_id: 2,
+                                            course_name: 'technopark 3 sem',
+                                            group_id: 2,
+                                            group_name: "AПО-31",
+                                            subjects: [
+                                                {subject_id: 4, subject_name: "Android", total: 100, mark_name: 'отл'},
+                                                {subject_id: 5, subject_name: "Security", total: 89, mark_name: 'отл'},
+                                                {subject_id: 6, subject_name: "Hightload", total: 49, mark_name: 'хор'}
+                                            ]
+                                        }
+                                    ];
+                                    resolve(data);
+                                });
                             break;
 
                         case 2:
